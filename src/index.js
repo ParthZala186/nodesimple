ccc0af518f182c0885bb2865f242cd10f289004a
Parth Zala <parth.zala@cygnet.com>
2022-02-03 14:35:57 +0530

    First Release for Demo Purpose of node js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+let express = require('express');
+const PORT = process.env.PORT || 80;
+let app = express()
+
+app.get("/",(req,res) =>{
+    res.send("Hello Parth")
+})
+
+app.use((req,res)=>{
+    res.status(400).send("Not Found")
+});
+
+app.listen(PORT,()=>{
+    console.log(`Listing on port ${PORT}`);
+})
